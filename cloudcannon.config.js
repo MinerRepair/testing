a36dbad4db7e087f942e5abfab7fab5198240a7f
colbyfayock <fayock@gmail.com>
2022-01-30 17:15:43 -0500

    pages config

--- cloudcannon.config.js
+++ cloudcannon.config.js
@@ -6,6 +6,13 @@ module.exports = {
       parser: 'frontmatter',
       output: true,
       url: '/posts/[slug]'
-    }
+    },
+    pages: {
+      name: 'Pages',
+      path: 'src/_pages',
+      parser: 'frontmatter',
+      output: true,
+      url: '/[slug]'
+    },
   }
 }
